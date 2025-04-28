51c475a7d07552a423f71af41368a3a7f834021e
Ron-Cola168 <pablohsg168@gmail.com>
2025-04-28 09:08:53 +0200

    creacion de clases

---
+++ src/Productos/Cartas.java
@@ -0,0 +1,23 @@
+package Productos;
+
+public class Cartas extends Producto{
+
+    private String juego;
+
+        //Constructor para Cartas
+    public Cartas(String nombre, int precio, String tipo, int stock, String juego){
+        super.Producto(nombre, precio, tipo, stock);
+        this.juego = juego;
+    }
+
+        //Getters y setters
+    public String getJuego(){
+        return this.juego;
+    }
+
+    public void setJuego(String juego) {
+        this.juego = juego;
+    }
+
+
+}
