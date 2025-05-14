5cef4b578e3c769a45e7ea76bedb6a532cb0c897
Ron-Cola168 <pablohsg168@gmail.com>
2025-05-14 13:45:27 +0200

    Clase empleado

---
+++ src/ClasesModelo/Empleado.java
@@ -0,0 +1,80 @@
+package ClasesModelo;
+
+public class Empleado {
+
+    private  int id;
+    private String nombre;
+    private String apellidos;
+    private String correo;
+    private String tipoEmpleado;
+    private String contraseña;
+
+    /**Constructores**/
+    public Empleado(){
+    }
+
+    public Empleado(String nombre, String apellidos, String tipoEmpleado, String contraseña){
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.tipoEmpleado = tipoEmpleado;
+        this.contraseña = contraseña;
+    }
+
+    public Empleado(int id,String nombre, String apellidos, String correo, String tipoEmpleado, String contraseña){
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.correo = correo;
+        this.tipoEmpleado = tipoEmpleado;
+        this.contraseña = contraseña;
+    }
+
+    /**Getters y Setters**/
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTipoEmpleado() {
+        return tipoEmpleado;
+    }
+
+    public void setTipoEmpleado(String tipoEmpleado) {
+        this.tipoEmpleado = tipoEmpleado;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+}
---
+++ src/ClasesModelo/Juego_de_mesa.java
@@ -0,0 +1,31 @@
+package ClasesModelo;
+
+public class Juego_de_mesa extends Producto{
+
+    private String editorial;
+    private int Numero_jugadores;
+
+        //Constructor para Juego de Mesa
+    public Juego_de_mesa(String nombre, int precio,String tipo ,String editorial, int stock, int Numero_jugadores){
+        super(nombre, precio, tipo, stock);
+        this.editorial = editorial;
+        this.Numero_jugadores = Numero_jugadores;
+    }
+
+        //Setters y getters
+    public String getEditorial(){
+        return this.editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public int getNumero_jugadores() {
+        return Numero_jugadores;
+    }
+
+    public void setNumero_jugadores(int numero_jugadores) {
+        Numero_jugadores = numero_jugadores;
+    }
+}
